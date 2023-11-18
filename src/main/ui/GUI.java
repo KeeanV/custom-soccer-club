70d6c32e0bafcda39bfad7e0b5d538b96c99c45e
keean <kvidyart@students.cs.ubc.ca>
2023-11-17 16:36:20 -0800

    edited actionListener

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -9,7 +9,10 @@ import javax.swing.*;
 
 public class GUI extends JFrame implements ActionListener {
     private static Club club1;
-    JButton button = new JButton("View the list of players in your club");
+    JButton button1 = new JButton("View the list of players in your club");
+    JButton button2 = new JButton("Save your club");
+    JButton button3 = new JButton("Add players");
+    JButton button4 = new JButton("View players");
 
     public GUI() {
         createGUI();
@@ -24,32 +27,25 @@ public class GUI extends JFrame implements ActionListener {
         frame.pack();
         frame.setVisible(true);
 
+
         JPanel panel = new JPanel();
 
 
-        panel.add(button);
-        button.setVisible(true);
+        panel.add(button1);
+        button1.setVisible(true);
+        button1.addActionListener(this);
 
-        button.addActionListener(this);
-//        button.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                doGetListOfPlayers();
-//            }
-//        });
-
-        JButton button2 = new JButton("Save your club");
         panel.add(button2);
         button2.setVisible(true);
+        button2.addActionListener(this);
 
-        JButton button3 = new JButton("Add players");
         panel.add(button3);
         button3.setVisible(true);
+        button3.addActionListener(this);
 
-        JButton button4 = new JButton("View players");
         panel.add(button4);
         button4.setVisible(true);
-
+        button4.addActionListener(this);
 
         frame.getContentPane().add(panel);
 
@@ -58,8 +54,17 @@ public class GUI extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if ((e.getSource()) == button) {
+        if ((e.getSource()) == button1) {
             doGetListOfPlayers();
+        }
+        if ((e.getSource()) == button2) {
+            //
+        }
+        if ((e.getSource()) == button3) {
+            //
+        }
+        if ((e.getSource()) == button4) {
+            //
         }
     }
 
