3ac382f6dc3e18c73256d05deaea9b0b63b4937d
keean <kvidyart@students.cs.ubc.ca>
2023-11-24 15:39:44 -0800

    documentation and refactoring

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -5,10 +5,13 @@ import model.Player;
 import model.WorkRoom;
 import persistence.JsonReader;
 import persistence.JsonWriter;
+import java.util.List;
 
 import java.awt.*;
 import java.awt.event.*;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,6 +23,7 @@ public class GUI extends JFrame implements ActionListener {
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
     private static Club club1;
+    private List<Player> myList;
     JButton button1 = new JButton("View the list of players in your club");
     JButton button2 = new JButton("Add a player to your club");
     JButton button3 = new JButton("Save your club");
@@ -29,8 +33,12 @@ public class GUI extends JFrame implements ActionListener {
     public GUI() {
         createGUI();
         club1 = new Club("Vancouver City");
+        jsonReader = new JsonReader(JSON_STORE);
+        jsonWriter = new JsonWriter(JSON_STORE);
+        myList = new ArrayList<>();
     }
 
+    // CITATION: GUI frame and panel setup based on code shown in: https://www.youtube.com/watch?v=dvzAuq-YDpM
     // EFFECTS: adds panels to JFrame and shows menu with buttons
     public void createGUI() {
         JLabel label = new JLabel();
@@ -43,9 +51,7 @@ public class GUI extends JFrame implements ActionListener {
         JPanel panel1 = new JPanel();
         panel1.setBackground(Color.cyan);
         panel1.setBounds(0, 0, 250, 800);
-        panel1.add(button1);
-        button1.setVisible(true);
-        button1.addActionListener(this);
+        createButton1(panel1, button1);
         panel1.add(button2);
         button2.setVisible(true);
         button2.addActionListener(this);
@@ -59,6 +65,12 @@ public class GUI extends JFrame implements ActionListener {
         panel2.setBounds(250, 0, 550, 800);
         panel2.setLayout(new BorderLayout());
 
+        createJFrame(label, panel1, panel2);
+    }
+
+    // REQUIRES: a label and two panel objects
+    // EFFECTS: creates a JFrame and adds labels and panels
+    private static void createJFrame(JLabel label, JPanel panel1, JPanel panel2) {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
@@ -69,6 +81,15 @@ public class GUI extends JFrame implements ActionListener {
         frame.add(panel2);
     }
 
+    // REQUIRES: panel and button objects
+    // EFFECTS: adds panel and button objects to the JFrame
+    private void createButton1(JPanel panel1, JButton button1) {
+        panel1.add(button1);
+        button1.setVisible(true);
+        button1.addActionListener(this);
+    }
+
+    // EFFECTS: assigns actions to different buttons when clicked
     @Override
     public void actionPerformed(ActionEvent e) {
         if ((e.getSource()) == button1) {
@@ -87,10 +108,16 @@ public class GUI extends JFrame implements ActionListener {
             }
         }
         if ((e.getSource()) == button4) {
-            //
+            try {
+                club1 = jsonReader.read();
+                updatePlayers();
+            } catch (IOException exception) {
+                //
+            }
         }
     }
 
+    // EFFECTS: creates a new JFrame that lists players in the club
     public static void button1() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -107,6 +134,7 @@ public class GUI extends JFrame implements ActionListener {
         panel1.add(doGetListOfPlayers());
     }
 
+    // EFFECTS: creates a new JFrame that prompts user input and stores that value in a string
     public static void button2() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -126,6 +154,7 @@ public class GUI extends JFrame implements ActionListener {
         club1.addPlayer(p1);
     }
 
+    // EFFECTS: returns a string of the list of players currently added to the club
     private static JLabel doGetListOfPlayers() {
         JLabel label = new JLabel();
         String names = "";
@@ -136,7 +165,15 @@ public class GUI extends JFrame implements ActionListener {
         return label;
     }
 
+    // EFFECTS: loads the list of players that was saved in the club
+    private void updatePlayers() {
+        myList.clear();
+        for (Player p : club1.getPlayers()) {
+            myList.add(p);
+        }
+    }
 
+    //EFFECTS: helper method to return a random boolean value for a clean sheet
     private static boolean getRandomBoolean() {
         if (Math.random() > 0.5) {
             return true;
