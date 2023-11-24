591d60ba1867ca8c2f79ecc4b1c0cea0374c4a95
keean <kvidyart@students.cs.ubc.ca>
2023-11-24 00:21:48 -0800

    added two panels, ball image, and four buttons

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -6,6 +6,8 @@ import model.Player;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI extends JFrame implements ActionListener {
     private static Club club1;
@@ -19,43 +21,55 @@ public class GUI extends JFrame implements ActionListener {
     }
 
     public void createGUI() {
-        JFrame frame = new JFrame("Soccer Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel emptyLabel = new JLabel(" ");
-        emptyLabel.setPreferredSize(new Dimension(500, 500));
-        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-        frame.pack();
-        frame.setVisible(true);
 
+        JLabel label = new JLabel();
+        label.setText("HELLO");
+        ImageIcon icon = new ImageIcon("ball.png");
+        label.setIcon(icon);
+        label.setVerticalAlignment(JLabel.TOP);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        //label.setBounds(100, 100, 75, 75);
 
-        JPanel panel = new JPanel();
-
-
-        panel.add(button1);
+        JPanel panel1 = new JPanel();
+        panel1.setBackground(Color.cyan);
+        panel1.setBounds(0, 0, 250, 800);
+        //panel1.setLayout(new BorderLayout());
+        panel1.add(button1);
         button1.setVisible(true);
         button1.addActionListener(this);
-
-        panel.add(button2);
+        panel1.add(button2);
         button2.setVisible(true);
         button2.addActionListener(this);
-
-        panel.add(button3);
+        panel1.add(button3);
         button3.setVisible(true);
         button3.addActionListener(this);
-
-        panel.add(button4);
+        JButton button4 = new JButton("View players");
+        panel1.add(button4);
         button4.setVisible(true);
-        button4.addActionListener(this);
 
-        frame.getContentPane().add(panel);
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.white);
+        panel2.setBounds(250, 0, 550, 800);
+        panel2.setLayout(new BorderLayout());
 
+
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(800, 800);
+        frame.setVisible(true);
+        panel2.add(label);
+        frame.add(panel1);
+        frame.add(panel2);
     }
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if ((e.getSource()) == button1) {
-            doGetListOfPlayers();
+
+            //doGetListOfPlayers();
+
         }
         if ((e.getSource()) == button2) {
             //
@@ -71,6 +85,8 @@ public class GUI extends JFrame implements ActionListener {
     private static void doGetListOfPlayers() {
         if (club1.getPlayers().size() > 0) {
             System.out.println("The following players are in your club: ");
+            //notes: use a new jpanel for each button, use text fields which have a get() method.
+
             for (Player player : club1.getPlayers()) {
                 System.out.println(player.getPlayerName());
             }
