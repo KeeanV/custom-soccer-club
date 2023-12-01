dbc0561c308e8fcb7f9777f22931d082748a1d1a
keean <kvidyart@students.cs.ubc.ca>
2023-12-01 15:11:48 -0800

    Phase 4: Task 3

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -4,40 +4,30 @@ import java.util.Calendar;
 import java.util.Date;
 
 
-/**
- * Represents an alarm system event.
- */
+// Represents an event occurring during the game
+// Based on AlarmSystem from GitHub
 public class Event {
     private static final int HASH_CONSTANT = 13;
     private Date dateLogged;
     private String description;
 
-    /**
-     * Creates an event with the given description
-     * and the current date/time stamp.
-     * @param description  a description of the event
-     */
+    // REQUIRES: a string describing the event
+    // EFFECTS: creates an event with the given description and the current date/time stamp.
     public Event(String description) {
         dateLogged = Calendar.getInstance().getTime();
         this.description = description;
     }
 
-    /**
-     * Gets the date of this event (includes time).
-     * @return  the date of the event
-     */
     public Date getDate() {
         return dateLogged;
     }
 
-    /**
-     * Gets the description of this event.
-     * @return  the description of the event
-     */
     public String getDescription() {
         return description;
     }
 
+    // REQUIRES: other != null
+    // EFFECTS: compares two events, this and other, and returns true if they are equal
     @Override
     public boolean equals(Object other) {
         if (other == null) {
@@ -53,11 +43,13 @@ public class Event {
                 this.description.equals(otherEvent.description));
     }
 
+    // EFFECTS: returns a hash code from the hash codes of dateLogged and description
     @Override
     public int hashCode() {
         return (HASH_CONSTANT * dateLogged.hashCode() + description.hashCode());
     }
 
+    // EFFECTS: returns a string representation of dateLogged
     @Override
     public String toString() {
         return dateLogged.toString() + "\n" + description;
--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-/**
- * Represents a log of alarm system events.
- * We use the Singleton Design Pattern to ensure that there is only
- * one EventLog in the system and that the system has global access
- * to the single instance of the EventLog.
- */
+
+// Represents a log of events occurring during runtime. We use the Singleton Design Pattern to ensure that there is
+    // one EventLog in the system and that the system has global access to the single instance of the EventLog.
+// Based on AlarmSystem from GitHub
 public class EventLog implements Iterable<Event> {
     /** the only EventLog in the system (Singleton Design Pattern) */
     private static EventLog theLog;
@@ -19,16 +17,12 @@ public class EventLog implements Iterable<Event> {
      * Prevent external construction.
      * (Singleton Design Pattern).
      */
+    // EFFECTS: creates an eventLog with an empty list of events
     private EventLog() {
         events = new ArrayList<Event>();
     }
 
-    /**
-     * Gets instance of EventLog - creates it
-     * if it doesn't already exist.
-     * (Singleton Design Pattern)
-     * @return  instance of EventLog
-     */
+    // EFFECTS: creates an instance of eventLog if the log does not exist yet and returns the eventLog
     public static EventLog getInstance() {
         if (theLog == null) {
             theLog = new EventLog();
@@ -36,27 +30,26 @@ public class EventLog implements Iterable<Event> {
         return theLog;
     }
 
-    /**
-     * Adds an event to the event log.
-     * @param e the event to be added
-     */
+    // REQUIRES: an event object
+    // EFFECTS: adds an event to the event log
     public void logEvent(Event e) {
         events.add(e);
     }
 
-    /**
-     * Clears the event log and logs the event.
-     */
+    // EFFECTS: clears the event log and logs the event
     public void clear() {
         events.clear();
         logEvent(new Event("Event log cleared."));
     }
 
+
+    // EFFECTS: returns an iterator that iterates over the list of events
     @Override
     public Iterator<Event> iterator() {
         return events.iterator();
     }
 
+    // EFFECTS: returns a string representation of the list of events
     @Override
     public String toString() {
         String message = "";
